83c732305eaf7aa4a32af4bd86f7e0407a2fc979
Aman Kr. Verma <aman901v@gmail.com>
2026-01-31 23:00:15 +0530

    Add files via upload

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgT6au0ArZPz7PJRVvX-Y9wOS_dOdPiPk",
+  authDomain: "attendify-d60c6.firebaseapp.com",
+  projectId: "attendify-d60c6",
+  storageBucket: "attendify-d60c6.firebasestorage.app",
+  messagingSenderId: "932362574776",
+  appId: "1:932362574776:web:3823f10a8d02fabce08d4d",
+  measurementId: "G-S1DYV1T6PL"
+};
+
+// Singleton initialization to prevent multiple instances
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
